f7dde92f896e95bdbc89fe1a14ad82ac7945b1cd
kzmw <kazumanombiri@icloud.com>
2024-11-30 17:52:11 +0900

    fix: 小数点丸め・コメントアウト修正・ログ追加

--- src/main/kotlin/net/nisylvania/discord_notify/PlayerListener.kt
+++ src/main/kotlin/net/nisylvania/discord_notify/PlayerListener.kt
@@ -1,5 +1,6 @@
 package net.nisylvania.discord_notify
 
+import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -14,31 +15,33 @@ class PlayerListeners(private val main: DiscordNotify) : Listener {
 
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
-        // プレイヤーの死亡イベント
+        // プレイヤーの入室イベント
         val player = event.player
         val location = player.location
         val worldName = location.world?.name ?: "不明なワールド"
-        val x = location.x
-        val y = location.y
-        val z = location.z
+        val x = String.format("%.1f",location.x)
+        val y = String.format("%.1f",location.y)
+        val z = String.format("%.1f",location.z)
 
         // 参加成功時メッセージ
         val message = "${player.name}が参加しました。\n座標：[${x}, ${y}, ${z}]（${worldName}）"
+        Bukkit.getLogger().info(message)
         main.notifyDiscord(message) // Discord通知
     }
 
     @EventHandler
     fun onPlayerQuit(event: PlayerQuitEvent) {
-        // プレイヤーの死亡イベント
+        // プレイヤーの退出イベント
         val player = event.player
         val location = player.location
         val worldName = location.world?.name ?: "不明なワールド"
-        val x = location.x
-        val y = location.y
-        val z = location.z
+        val x = String.format("%.1f",location.x)
+        val y = String.format("%.1f",location.y)
+        val z = String.format("%.1f",location.z)
 
         // 参加成功時メッセージ
         val message = "${player.name}が退出しました。\n座標：[${x}, ${y}, ${z}]（${worldName}）"
+        Bukkit.getLogger().info(message)
         main.notifyDiscord(message) // Discord通知
     }
 
@@ -49,7 +52,8 @@ class PlayerListeners(private val main: DiscordNotify) : Listener {
             val reason = event.kickMessage
 
             // ログイン失敗時メッセージ
-            val message = "⚠\uFE0F${playerName}が${reason}のためログインに失敗しました。"
+            val message = "${playerName}が${reason}のためログインに失敗しました。"
+            Bukkit.getLogger().info(message)
             main.notifyDiscord(message) // Discord通知
 
         }
@@ -62,7 +66,8 @@ class PlayerListeners(private val main: DiscordNotify) : Listener {
             val reason = event.kickMessage
 
             // ログイン拒否時メッセージ
-            val message = "\uD83D\uDEAB${playerName}が${reason}のためログインに失敗しました。"
+            val message = "${playerName}が${reason}のためログインに失敗しました。"
+            Bukkit.getLogger().info(message)
             main.notifyDiscord(message) // Discord通知
 
         }
@@ -74,13 +79,14 @@ class PlayerListeners(private val main: DiscordNotify) : Listener {
         val player = event.entity
         val location = player.location
         val worldName = location.world?.name ?: "不明なワールド"
-        val x = location.x
-        val y = location.y
-        val z = location.z
+        val x = String.format("%.1f",location.x)
+        val y = String.format("%.1f",location.y)
+        val z = String.format("%.1f",location.z)
         val deathCause = player.lastDamageCause?.cause?.name ?: "不明な死因"
 
         // 死亡時メッセージ
         val message = "${player.name}が死亡しました。\n死因：${deathCause}\n座標：[${x}, ${y}, ${z}]（${worldName}）"
+        Bukkit.getLogger().info(message)
         main.notifyDiscord(message) // Discord通知
     }
 }
